6117cb23f048dfa376ac3fd74f9873a019bc0eda
Adi M. Yasir <adimuhammadyasir93@gmail.com>
2025-11-22 19:20:13 +0700

    feat: fix add grouping for routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -10,15 +10,22 @@ import (
 
 func New(h *handler.Handler) chi.Router {
 	r := chi.NewRouter();
+
 	r.Route("/api/v1", func(r chi.Router) {
-		r.Post("/auth/login", h.Auth.Login);
+		// Public routes
+		r.Group(func(r chi.Router) {
+			r.Post("/auth/login", h.Auth.Login);
+		})
 
-		r.Use(jwtauth.Verifier(lib.TokenAuth));
-		r.Use(jwtauth.Authenticator(lib.TokenAuth));
+		// Protected routes
+		r.Group(func(r chi.Router) {
+			r.Use(jwtauth.Verifier(lib.TokenAuth));
+			r.Use(jwtauth.Authenticator(lib.TokenAuth));
 
-		r.Route("/users", func(r chi.Router) {
-			r.Post("/", h.User.CreateUser);
-		});
+			r.Route("/users", func(r chi.Router) {
+				r.Post("/", h.User.CreateUser);
+			});
+		})
 	});
 	return r;
 }
